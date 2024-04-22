8f1273d00be186982c9e029a0aec5a1cea36a445
Botond Prg <e12347325@student.tuwien.ac.at>
2024-04-22 17:31:20 +0000

    reactive init

---
+++ reactive/reactive/reactive.py
@@ -0,0 +1,77 @@
+#!/usr/bin/env python3
+import math
+import rclpy
+
+from rclpy.node import Node
+from rclpy.qos import QoSProfile
+from sensor_msgs.msg import LaserScan
+from nav_msgs.msg import Odometry
+from geometry_msgs.msg import Twist
+from ackermann_msgs.msg import AckermannDriveStamped
+from diagnostic_msgs.msg import DiagnosticArray, DiagnosticStatus, KeyValue
+from std_srvs.srv import Trigger
+from std_msgs.msg import Float32  # For publishing velocity, TTC, and distance for PlotJuggler
+from visualization_msgs.msg import Marker  # For creating and updating the spherical marker
+
+class Reactive(Node):
+    """
+    This node implements the "Follow the gap" algo
+    """
+    def __init__(self):
+        super().__init__('reactive')
+        
+        # Publishers
+        self.drive_publisher = self.create_publisher(AckermannDriveStamped, '/drive', 10)
+        self.marker_pub = self.create_publisher(Marker, '/viz_marker', 10)
+        self.marker_pub0 = self.create_publisher(Marker, '/viz_marker0', 10)
+        self.marker_pub1 = self.create_publisher(Marker, '/viz_marker1', 10)
+        self.marker_pub2 = self.create_publisher(Marker, '/viz_marker2', 10)
+        self.marker_pub3 = self.create_publisher(Marker, '/viz_marker3', 10)
+        self.marker_pub4 = self.create_publisher(Marker, '/viz_marker4', 10)
+        self.marker_scan = self.create_publisher(LaserScan, '/viz_scan', 10)
+        
+        # Subscribers
+        qos = QoSProfile(depth=10)
+        self.create_subscription(LaserScan, '/scan', self.scan_callback, qos)
+    
+    def publish_drive(self, steering_angle, speed):
+        drive_msg = AckermannDriveStamped()
+        drive_msg.drive.speed = speed
+        drive_msg.drive.steering_angle = steering_angle
+        self.drive_publisher.publish(drive_msg)
+    
+    def scan_callback(self, scan_msg: LaserScan):
+        pass
+        speed = 0.5
+        steering_angle = 0
+        # implement follow the gap algo
+        # obtain laser scans, preprocess
+        rng_proc = scan_msg.ranges
+        # find closes point
+        min_rng = min(rng_proc)
+        idx = rng_proc.index(min_rng)
+        rng_proc[idx] = 0
+        
+        # draw a "safety bubble" around it
+        for i in range(1, 20, 1):
+            left_idx = idx-i
+            right_idx = idx+i
+            rng_proc[left_idx] = 0
+            rng_proc[right_idx] = 0
+        
+        # find the max length gap
+        
+                
+        
+        
+        self.publish_drive(steering_angle, speed)
+    
+def main(args=None):
+    rclpy.init(args=args)
+    reactive = Reactive()
+    rclpy.spin(reactive)
+    reactive.destroy_node()
+    rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
---
+++ reactive/setup.py
@@ -0,0 +1,31 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'reactive'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.py')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.xacro')),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.rviz')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='T4',
+    maintainer_email='T4@T4.com',
+    description='Follow the gap mechanism',
+    license='License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
