aeeae904bd5150400a0bc75adfcdb1a81859cb3a
PeteHeat08 <pero.drobac08@gmail.com>
2024-05-10 17:09:14 +0200

    Incomplete version of pursuit algorithm

--- pursuit/pursuit/pursuit.py
+++ pursuit/pursuit/pursuit.py
@@ -6,18 +6,20 @@ from rclpy.node import Node
 from rclpy.qos import QoSProfile
 from sensor_msgs.msg import LaserScan
 from nav_msgs.msg import Odometry
-from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Point, Pose
 from ackermann_msgs.msg import AckermannDriveStamped
-from diagnostic_msgs.msg import DiagnosticArray, DiagnosticStatus, KeyValue
-from std_srvs.srv import Trigger
-from std_msgs.msg import Float32  # For publishing velocity, TTC, and distance for PlotJuggler
-from visualization_msgs.msg import Marker  # For creating and updating the spherical marker
 
 class Pursuit(Node):
     """
     The class that handles emergency braking with enhancements for data visualization and dynamic parameter adjustment.
     """
     def __init__(self):
+        # Parameters
+        self.lookahead_distance = 1.0
+        self.waypoint_index = 0
+
+        # TODO define type and fill with values
+        self.path = [] # array of points?
 
         # Publishers
         self.drive_publisher = self.create_publisher(AckermannDriveStamped, '/drive', 10)
@@ -27,11 +29,37 @@ class Pursuit(Node):
         self.create_subscription(LaserScan, 'scan', self.scan_callback, qos)
         self.create_subscription(Odometry, 'odom', self.odom_callback, qos)
 
-    def odom_callback(self, odom_msg: Odometry):
-        pass
+    def publish_drive(self, steering_angle, speed):
+        drive_msg = AckermannDriveStamped()
+        drive_msg.drive.speed = speed
+        drive_msg.drive.steering_angle = steering_angle
+        self.drive_publisher.publish(drive_msg)
 
-    def scan_callback(self, scan_msg: LaserScan):
-        pass
+    def odom_callback(self, odom_msg: Odometry):
+        # Helper variables
+        position = odom_msg.pose.pose.position
+        furthest_distance = 0
+        
+        # TODO define type, Point placeholder
+        furthest_waypoint = Point()
+        visible_waypoints = []
+
+        # Find furthest waypoint
+        for waypoint in self.path[self.waypoint_index:]:
+            distance = math.sqrt((position.x - waypoint.x)**2 + (position.y - waypoint.y)**2)
+            if distance < self.lookahead_distance:
+                visible_waypoints.append(waypoint)
+                if distance > furthest_distance:
+                    furthest_waypoint = waypoint
+                    self.waypoint_index = self.path.index(waypoint)
+
+        # Calculate angle to furthest waypoint
+        angle_desired = math.atan2(furthest_waypoint.y - position.y, furthest_waypoint.x - position.x)
+        angle_current = odom_msg.pose.pose.orientation.z
+        steering_angle = angle_desired - angle_current
+
+        # Publish drive command
+        self.publish_drive(steering_angle, 1.0)
 
 def main(args=None):
     rclpy.init(args=args)
